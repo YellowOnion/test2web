ee7b85cb0f87c0a564a98859a7e9745a8d9f40bf
Daniel Hill <daniel@gluo.nz>
2022-06-28 17:51:55 +1200

    Simplify code, performance improvements.
    Collections have been removed, and use one big table now.
    
    We read from the end of the file to improve performance.
    
    Signed-off-by: Daniel Hill <daniel@gluo.nz>

--- app/Main.hs
+++ app/Main.hs
@@ -17,64 +17,40 @@ import qualified Text.Blaze.Html5 as H
 import           Text.Blaze.Html5 ((!))
 import qualified Text.Blaze.Html5.Attributes as A
 import Text.Blaze.Html.Renderer.Pretty ( renderHtml )
-import Text.Blaze.Html4.FrameSet (body, col)
 
-import Debug.Trace
+import qualified Debug.Trace as Trace
 import Data.HashMap.Strict (foldMapWithKey)
 import GHC.Base (Semigroup)
+
+
+trace' a = Trace.trace (show a) a
 
 data TestResult = Pass | Fail
   deriving (Show)
 
 data Test = Test
   { testName :: String,
-    testCollection :: String,
     testResult :: Maybe TestResult,
     testTime :: Maybe Int
   } deriving (Show)
 
 instance H.ToMarkup Test where
-  toMarkup (Test n c (Just r) t) = H.tr ! resultColor r $ do
+  toMarkup (Test n (Just r) t) = H.tr ! resultColor r $ do
     H.td $ H.toHtml n
     H.td . H.toHtml $ show r
     H.td . H.toHtml $ case t of
                         Nothing -> "Timeout"
                         Just t -> show t ++ "s"
-    H.td $ H.a ! A.href (H.toValue $ c ++ "." ++ n) $ "log"
+    H.td $ H.a ! A.href (H.toValue n) $ "log"
     where
       resultColor Pass = A.class_ "table-success"
       resultColor Fail = A.class_ "table-danger"
 
-newtype TestCollections = TestCollections
-  { toMap :: Map.HashMap String [Test]
-  }
 
---instance Functor TestCollections where
---  fmap f = Map.map f . toMap
-instance Semigroup TestCollections where
- a <> b = TestCollections $ Map.unionWith (<>) (toMap a) (toMap b)
-
-instance Monoid TestCollections where
-  mempty = TestCollections Map.empty
-  mappend = (<>)
-
-  -- | XfsTests probably need to treat xfstests speically.
-
-testMaybeInsert :: String -> Maybe Test -> TestCollections -> TestCollections
-testMaybeInsert _ Nothing = id
-testMaybeInsert col test = TestCollections . Map.alter alterDo col . toMap
+parseTests :: String -> [String] -> [Test]
+parseTests name = mapMaybe parseTest
   where
-    alterDo Nothing   = fmap (:[]) test
-    alterDo _ = error "This shouldn't happen"
-
-fileToTest fs = Test a b Nothing Nothing
-  where (b, a) = splitExtension fs
-
-
-parseTests :: (String, String) -> [String] -> TestCollections
-parseTests (col, _:name) = foldr parseTest mempty
-  where
-    parseTest = testMaybeInsert col . go ( Test name col Nothing Nothing )
+    parseTest = go ( Test name Nothing Nothing )
     go _ "" = Nothing
     go t cs@(c:s)
       | isDigit c || c == ' ' || c == '='           = go t s
@@ -82,7 +58,7 @@ parseTests (col, _:name) = foldr parseTest mempty
       | c == 'F' && "FAILED " `isSubsequenceOf` cs  =  Just $ parseNT (t { testResult = Just Fail }) (drop 6 s)
       | c == 'T' && "TEST FAILED " `isSubsequenceOf` cs  = Just (t { testResult = Just Fail
                                                               })
-      | otherwise =  Nothing
+      | otherwise = error name
     parseNT t s = t2
         where
           (t1, s1) = parseName t s
@@ -91,7 +67,7 @@ parseTests (col, _:name) = foldr parseTest mempty
     parseName t s = (t, dropWhile (/= ' ') s)
 
 
-testsToHtml :: String -> TestCollections -> H.Html
+testsToHtml :: String -> [Test] -> H.Html
 testsToHtml rev tests = H.docTypeHtml $ do
   H.head $ do
     H.title $ H.toHtml ("Tests for rev: " ++ rev)
@@ -100,9 +76,20 @@ testsToHtml rev tests = H.docTypeHtml $ do
   H.body $ do
     H.div ! A.class_ "container" $ do
       H.h1 $ H.toHtml ("Rev: " ++ rev)
-      Map.foldMapWithKey (\name subtests -> do
-                            H.h2 (H.toHtml name)
-                            H.table ! A.class_ "table" $ foldMap H.toHtml subtests) $ toMap tests
+      (H.table ! A.class_ "table") $ foldMap H.toHtml tests
+
+readLastLine :: Handle -> IO String
+readLastLine h =  hSeek h SeekFromEnd 0 >> go False ""
+  where go a s = do
+          n <- hTell h
+          if n <= 0 then return s
+            else do
+              hSeek h RelativeSeek (-1)
+              c <- hGetChar h
+              hSeek h RelativeSeek (-1)
+              if not ( c == '\n' && a)
+                then go (c /= '\n') (c:s)
+                else return s
 
 
 main :: IO ()
@@ -115,6 +102,6 @@ main = do
       fs <- listDirectory directory
       tests <- flip foldMap ( filter (\a -> elem '.' a || "index.html" == a) fs) $ \fileName -> do
         hPutStrLn stderr fileName
-        fc <- readFile $ directory </> fileName
-        return (parseTests (splitExtension fileName) . lines $ fc)
+        fc <- withFile (directory </> fileName) ReadMode readLastLine
+        return (parseTests fileName . lines $ fc)
       putStr . renderHtml . testsToHtml rev $ tests
